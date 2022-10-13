b3891a9cb67556c11d9c338503fe9cf95cb035ee
zksytmkn <zksytmkn@example.com>
2022-10-13 17:21:44 +0000

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,5 +13,9 @@ class ActiveSupport::TestCase
   include ApplicationHelper
 
 
-  # Add more helper methods to be used by all tests here...
+  # Add more helper methods to be used by all tests here
+    # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
