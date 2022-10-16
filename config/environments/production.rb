38d3490767db4f90d4522c0b45e3f3c39dd1119b
zksytmkn <zksytmkn@example.com>
2022-10-16 07:15:02 +0000

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'heltered-sands-07628.herokuapp.com'
+  host = 'sheltered-sands-07628.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
