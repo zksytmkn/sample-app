389c4f89f6b1f0b855276bab47cc0114db6d3350
zksytmkn <zksytmkn@example.com>
2022-10-18 08:55:41 +0000

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,12 @@
-# メインのサンプルユーザーを1人作成する
+# ユーザー
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true,
+             admin:     true,
              activated: true,
              activated_at: Time.zone.now)
 
-# 追加のユーザーをまとめて生成する
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
@@ -18,11 +17,19 @@ User.create!(name:  "Example User",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
-end   
-            
-# ユーザーの一部を対象にマイクロポストを生成する
+end
+
+# マイクロポスト
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# 以下のリレーションシップを作成する
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
